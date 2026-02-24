1387e49ca56bd90a973b57a364ea79d60afb6edf
alokshukla144 <alok.shukla@sourcemash.com>
2026-02-24 11:28:10 +0530

    removed unused lines

--- shipmentmgmt_WebapiCore/BusinessLayer/IRepsitory/Ishipmentitem.cs
+++ shipmentmgmt_WebapiCore/BusinessLayer/IRepsitory/Ishipmentitem.cs
@@ -13,11 +13,7 @@ namespace shipmentmgmt_WebapiCore.BusinessLayer.IRepsitory
         IEnumerable<shipmentitem> GetshipmentitemByID(int ID);
 
 
-<<<<<<< HEAD
-        //Test Push 3 ,4 ,5
-=======
-        //Test Push 3 ,4
->>>>>>> main
+
 
     }
 }
